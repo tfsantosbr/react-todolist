e9d0fe6948bd7787ce550ffad13b703e532d40f5
Tiago Santos <tfsantosbr@gmail.com>
2026-01-28 22:39:47 -0300

    refactor: remove biome configuration and update dependencies
    - Deleted biome.json configuration file.
    - Removed @biomejs/biome from package.json and pnpm-lock.yaml.
    - Added vite-plugin-svgr to package.json and pnpm-lock.yaml.
    - Updated vite.config.ts to include svgr plugin in the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react-swc"
+import svgr from "vite-plugin-svgr"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [react(), tailwindcss(), svgr()],
+})
